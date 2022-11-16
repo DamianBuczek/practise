a8cba9acabdbaee2de948141ea15f661f1b456ec
Uzytkownik <uzytkownik@kursGIT.pl>
2022-11-16 21:01:08 +0100

    Feature_change_locations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,10 @@ public class Main {
     public static void main(String[] args) {
 
         final LocationEntity location1 = new LocationEntity(1668453798, 46.5357,  22.4044);
-        final LocationEntity location2 = new LocationEntity(1668453798, 46.5357,  22.4044);
-        final LocationEntity location3 = new LocationEntity(1668453798, 46.5357,  22.4044);
+        final LocationEntity location2 = new LocationEntity(1668454557, 44.3262, 91.8486);
+        final LocationEntity location3 = new LocationEntity(1668454610, 42.5446, 95.6753);
+        final LocationEntity location4 = new LocationEntity(1668454618, 42.2636, 96.2323);
+        final LocationEntity location5 = new LocationEntity(1668454624, 42.0509, 96.6466);
 
 
     }
